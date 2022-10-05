15e12476778f4a484d7809283e48f8a24a9b8e84
kbekj <95280227+kbekj@users.noreply.github.com>
2022-10-05 15:19:03 +0200

    =?

--- server/timeServer.go
+++ server/timeServer.go
@@ -1,7 +1,7 @@
 package main
 
 import(
-	gRPC "go_time_service/proto"
+	gRPC "https://github.com/kbekj/go_time_service/proto"
 	//"google.golang.org/grpc"
 )
 
@@ -11,3 +11,17 @@ type TimeServer struct {
     
     // here you can impliment other fields that you want
 }
+
+func (s *Server) <endpoint name>(ctx context.Context, Time *gPRC.Time) (*gRPC.Hi, error) {
+    //some code here
+    ...
+    ack :=  // make an instance of your return type
+    return (ack, nil)
+}
+
+grpcServer := grpc.NewServer(opts...)
+server := &Server{
+    // set fields here 
+}
+gRPC.RegisterTemplateServer(grpcServer, server)
+grpcServer.Serve(list)
